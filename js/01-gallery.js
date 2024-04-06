4a7c4a378d7ad26e431002718f8c2820e8662824
Rostyslav-Fomin <142998754+Rostyslav-Fomin@users.noreply.github.com>
2024-04-06 08:25:22 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+galleryList.addEventListener('click', onImage);
+
+function onImage(event) {
+	console.log(event.currentTurget)
+}
+
+const items = [];
+
+galleryItems.map(({description, original, preview }) => {
+	document.createElement()
+})
+
+
+
+
