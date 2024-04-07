96a7a3862e6f047e813c755aa81860e265837d71
Rostyslav-Fomin <142998754+Rostyslav-Fomin@users.noreply.github.com>
2024-04-07 22:17:16 +0300

    corrected 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 
 const galleryList = document.querySelector('.gallery');
+
 galleryList.addEventListener('click', onImage);
 
-function onImage(event) {
-	console.log(event.currentTurget)
+function onImage({target}) {
+	if (!target.classList.contains('gallery__image')) {
+		return
+	} const instance = basicLightbox.create(document.querySelector('.gallery__image'));
+	console.log(instance)
+	target.src = target.dataset.src
+	instance.show()
 }
 
-const items = [];
+let items = '';
 
 galleryItems.map(({description, original, preview}) => {
-	const element = `<li><img class="img" src="${preview}" alt="${description} data-src="${original}"></li>`;
-	items.push(element)
+	const element = `<li class="gallery__item"><img class="gallery__image" src="${preview}" alt="${description}" data-src="${original}"></li>`;
+	items = items + element;
 })
-galleryList.insertAdjacentHTML('beforeend', ...items)
-console.log(galleryList)
-
+galleryList.insertAdjacentHTML('beforeend', items)
