34561438203277e8b70d139eb05fdac41ba4143d
Rostyslav-Fomin <142998754+Rostyslav-Fomin@users.noreply.github.com>
2024-04-11 08:59:42 +0300

    completed task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,11 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryList = document.querySelector('.gallery');
+
 const items = galleryItems.map(({ description, original, preview }) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a></li>`).join('');
 galleryList.insertAdjacentHTML('beforeend', items);
-const lightbox = new SimpleLightbox('.gallery a');
+
+var lightbox = new SimpleLightbox('.gallery a', {});
+lightbox.defaultOptions.captionsData = "alt";
+
+
